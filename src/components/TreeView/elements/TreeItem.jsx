7961a2931f057d834e3f5acbc6f2e089303ee20e
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-09-16 18:25:16 -0500

    Separe svg curve from css to tree item component

--- src/components/TreeView/elements/TreeItem.jsx
+++ src/components/TreeView/elements/TreeItem.jsx
@@ -15,6 +15,10 @@ function TreeItem ({ variant = 'default', className, isBranch = false, children,
   return (
     <Wrapper element={!isBranch && 'li'}>
       <div className={classes} {...props}>
+        <svg className='tree-curve' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 41 51.5' width='20px' fill='var(--color-lines)'>
+          <path d='M41,51.5H23.6C10.6,51.5,0,40.9,0,27.9L0.4,0h3.1L4,27.9c0,10.8,8.8,19.6,19.6,19.6H41V51.5z' />
+        </svg>
+
         <VariantItem>
           {children}
         </VariantItem>
--- src/pages/TreeViewPage.jsx
+++ src/pages/TreeViewPage.jsx
@@ -19,6 +19,11 @@ function TreeViewPage () {
               <TreeItem>Item 2.5.1</TreeItem>
               <TreeItem>Item 2.5.2</TreeItem>
               <TreeItem>Item 2.5.3</TreeItem>
+              <TreeBranch title='Item 2.5.4'>
+                <TreeItem>Item 2.5.4.1</TreeItem>
+                <TreeItem>Item 2.5.4.2</TreeItem>
+                <TreeItem>Item 2.5.4.3</TreeItem>
+              </TreeBranch>
             </TreeBranch>
           </TreeBranch>
           <TreeItem variant='solids'>Item 3</TreeItem>
@@ -28,7 +33,7 @@ function TreeViewPage () {
       </div>
       <div className='w-full max-w-3xl'>
         <h2 className='font-semibold text-3xl p-3 text-zinc-400'>Reference</h2>
-        <img className='w-' src='image_processing20211118-15492-16gegj.webp' alt='Reference' srcset='' />
+        <img className='w-' src='image_processing20211118-15492-16gegj.webp' alt='Reference' />
       </div>
     </main>
   )
