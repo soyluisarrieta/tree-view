623990729b0e5d1e117b01823a2afc562365c3a2
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-09-16 08:57:16 -0500

    Install and configure tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 function App () {
   return (
-    <>
-      <h1>Hi!</h1>
-    </>
+    <div className='min-h-screen bg-zinc-900 py-5'>
+      <h1 className='text-center text-4xl font-bold text-sky-500'>Hi Tailwind!</h1>
+    </div>
   )
 }
 
